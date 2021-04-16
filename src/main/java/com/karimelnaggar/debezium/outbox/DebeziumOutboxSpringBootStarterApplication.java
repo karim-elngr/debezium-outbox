3a85bf226619d5fea2cd682293e08c6d6529612b
Karim El-Naggar <karimfarid.naggar@gmail.com>
2021-04-17 00:03:41 +0200

    init project

---
+++ src/main/java/com/karimelnaggar/debezium/outbox/DebeziumOutboxSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.karimelnaggar.debezium.outbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebeziumOutboxSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebeziumOutboxSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karimelnaggar/debezium/outbox/DebeziumOutboxSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karimelnaggar.debezium.outbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebeziumOutboxSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
